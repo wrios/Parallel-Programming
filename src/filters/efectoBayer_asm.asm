22c77112548cfaecb9639a92f721913250bc0454
Agustin Sansone <asansone@dc.uba.ar>
2018-10-07 22:53:21 -0300

    Update efectoBayer_asm.asm

--- src/filters/efectoBayer_asm.asm
+++ src/filters/efectoBayer_asm.asm
@@ -2,9 +2,11 @@ global efectoBayer_asm
 
 section .data
 ;mask_rvrv : db 0xFF, 0xFF, 0x2, 0xFF, 0xFF, 0xFF, 0xFF, 0X7, 0xFF, 0xFF, 0XA, 0xFF, 0xFF, 0xFF, 0xFF, 0XF
-mask_rvrv : db  0xFF, 0xFF, 0x2, 0xFF,		0xFF, 0xFF, 0xFF, 0x7, 		0xFF, 0xFF, 0xA, 0xFF, 		0xFF, 0xD, 0xFF, 0xFF
+;mask_rvrv : db  0xFF, 0xFF, 0x2, 0xFF,		0xFF, 0x5, 0xFF, 0xFF, 		0xFF, 0xFF, 0xA, 0xFF, 		0xFF, 0xD, 0xFF, 0xFF
+mask_rvrv : db  0xFF, 0xFF, 0x2, 0xFF,		0xFF, 0xFF, 0x6, 0xFF, 		0xFF, 0x9, 0xFF, 0xFF, 		0xFF, 0xD, 0xFF, 0xFF
 
-mask_vava : db 0xFF, 0x1, 0xFF, 0xFF, 0xFF, 0xFF, 0X6, 0xFF, 0xFF, 0X9, 0xFF, 0xFF, 0xFF, 0xFF, 0XE, 0xFF
+;mask_vava : db 0xFF, 0x1, 0xFF, 0xFF, 0xFF, 0xFF, 0X6, 0xFF, 0xFF, 0X9, 0xFF, 0xFF, 0xFF, 0xFF, 0XE, 0xFF
+mask_vava : db 0xFF, 0xFF, 0xFF, 0xFF,		0xFF, 0xFF, 0xFF, 0xFF,		0xFF, 0xFF, 0xFF, 0xFF,		0xFF, 0xFF, 0xFF, 0xFF
 
 section .text
 efectoBayer_asm:
@@ -28,93 +30,67 @@ efectoBayer_asm:
 	ciclo_filas_bayer:
 		cmp r10d, edx; r10d = fila actual
 		je termino_ciclo_filas_bayer
-		;recorro esta fila
-			mov r11d, 0
-			ciclo_fila_actual:
-			
-			cmp r11d, ecx; r11d = columna actual
-			je termino_ciclo_fila_actual
-
-			movdqu xmm0, [rdi]; xmm0 = [rgba|rgba|rgba|rgba]
-			pshufb xmm0, xmm10; xmm0 = [r000|0g00|r000|0g00]
-			;se lo cargo a la img out
-			movdqu [rsi], xmm0;;
-			;sigo iterando
-			add rsi, 16;;
-			add rdi, 16;;
-			inc r11d
-			jmp ciclo_fila_actual
-		termino_ciclo_fila_actual:
-
-		;inc fila y veo si era la ultima
-		inc r10d
-		cmp r10d, edx
-		je termino_ciclo_filas_bayer
 		
-			;;hago exactamente lo mismo para la proxima fila
-				mov r11d, 0
-			ciclo_fila_actual_repito:
+			mov r8, 0
+			ciclo_4_veces:
+			cmp r8, 4
+			je termino_ciclo_4_veces
+				;recorro esta fila
+					mov r11d, 0
+					ciclo_fila_actual:
+					
+					cmp r11d, ecx; r11d = columna actual
+					je termino_ciclo_fila_actual
+
+					movdqu xmm0, [rdi]; xmm0 = [rgba|rgba|rgba|rgba]
+					pshufb xmm0, xmm10; xmm0 = [r000|0g00|r000|0g00]
+					;se lo cargo a la img out
+					movdqu [rsi], xmm0;;
+					;sigo iterando
+					add rsi, 16;;
+					add rdi, 16;;
+					inc r11d
+					jmp ciclo_fila_actual
+				termino_ciclo_fila_actual:
+
+				;inc fila y veo si era la ultima
+				inc r10d
+				cmp r10d, edx
+				je termino_ciclo_filas_bayer
+				inc r8
+				jmp ciclo_4_veces
+		termino_ciclo_4_veces:
+		
+		mov r8, 0
+		ciclo_4_veces_2:
+		cmp r8, 4
+		je termino_ciclo_4_veces_2
+				;recorro la siguiente fila
+					mov r11d, 0
+					ciclo_fila_actual_2:
+					cmp r11d, ecx; r11d = col actual
+					je termino_ciclo_fila_actual_2
+					
+					movdqu xmm0, [rdi]; xmm0 = [rgba|rgba|rgba|rgba]
+					pshufb xmm0, xmm11; xmm0 = [0g00|00b0|0g00|00b0]
+					;se lo cargo a la img out
+					movdqu [rsi], xmm0;;
+					;si sigo iterando
+					add rsi, 16;;
+					add rdi, 16;;
+					inc r11d
+					jmp ciclo_fila_actual_2
+				termino_ciclo_fila_actual_2:
 				
-				cmp r11d, ecx; r11d = columna actual
-				je termino_ciclo_fila_actual_repito
+				;inc fila y veo si era la ultima
+				inc r10d
+				cmp r10d, edx
+				je termino_ciclo_filas_bayer
+				inc r8
+				jmp ciclo_4_veces_2
+		termino_ciclo_4_veces_2:
 
-				movdqu xmm0, [rdi]; xmm0 = [rgba|rgba|rgba|rgba]
-				pshufb xmm0, xmm10; xmm0 = [r000|0g00|r000|0g00]
-				;se lo cargo a la img out
-				movdqu [rsi], xmm0;;
-				;sigo iterando
-				add rsi, 16;;
-				add rdi, 16;;
-				inc r11d
-				jmp ciclo_fila_actual_repito
-			termino_ciclo_fila_actual_repito:
-			;inc fila y veo si era la ultima
-			inc r10d
-			cmp r10d, edx
-			je termino_ciclo_filas_bayer
-		
-		;recorro la siguiente fila
-			mov r11d, 0
-			ciclo_fila_actual_2:
-			cmp r11d, ecx; r11d = col actual
-			je termino_ciclo_fila_actual_2
-			
-			movdqu xmm0, [rdi]; xmm0 = [rgba|rgba|rgba|rgba]
-			pshufb xmm0, xmm11; xmm0 = [0g00|00b0|0g00|00b0]
-			;se lo cargo a la img out
-			movdqu [rsi], xmm0;;
-			;si sigo iterando
-			add rsi, 16;;
-			add rdi, 16;;
-			inc r11d
-			jmp ciclo_fila_actual_2
-		termino_ciclo_fila_actual_2:
-		
-			;inc fila y veo si era la ultima
-			inc r10d
-			cmp r10d, edx
-			je termino_ciclo_filas_bayer
-			;;hago exactamente lo mismo para la proxima fila
-				mov r11d, 0
-			ciclo_fila_actual_2repito:
-				
-				cmp r11d, ecx; r11d = columna actual
-				je termino_ciclo_fila_actual_2repito
-
-				movdqu xmm0, [rdi]; xmm0 = [rgba|rgba|rgba|rgba]
-				pshufb xmm0, xmm11; xmm0 = [0g00|00b0|0g00|00b0]
-				;se lo cargo a la img out
-				movdqu [rsi], xmm0;;
-				;sigo iterando
-				add rsi, 16;;
-				add rdi, 16;;
-				inc r11d
-				jmp ciclo_fila_actual_2repito
-			termino_ciclo_fila_actual_2repito:
-		
-		;incremento fila
-		inc r10d
-		;vuelvo a hacer "dos*2" filas
+		;vuelvo a hacer ""dos"" filas
 		jmp ciclo_filas_bayer
 		
 	termino_ciclo_filas_bayer:
@@ -122,7 +98,6 @@ efectoBayer_asm:
 		;fin
 		pop rbp
 		ret
-
 
 
 
